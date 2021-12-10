ad59bfce21e059c8ee6a4a97fbb3e604cf1e4e72
orapra <94683574+orapra@users.noreply.github.com>
2021-12-10 15:38:10 +0330

    Create index.js

---
+++ land/index.js
@@ -0,0 +1,87 @@
+function sayHello() {
+document.getElementById('x').style.display='none';
+}
+function say() {
+document.getElementById('x').style.display='block';
+}
+
+function ldd() {
+document.write(`
+   <!DOCTYPE html>
+   <html>
+   <head>
+   <!-- CSS only -->
+   <!-- CSS only -->
+   
+   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=0.1, minimum-scale=0.97">
+   <link rel="stylesheet" href="https://as-nxm.github.io/css/min.css">
+   <title>همین اعلان شروع کن</title>
+   </head>
+   <body style="background:#fafafa;">
+   
+   <style>
+   *::selection{
+   background-color:#398;
+   
+   }
+   .download{
+   background-color:#2980B9; color:#fff; width:70%; height:53px; border-radius:1px; border: 1px solid #2980B9;
+   
+   padding:10px 25px;
+   }
+   .download:focus{
+   background:#2980B9;
+   color:#fff;
+   transition:0.1s;
+   }
+   .s{
+   color:#fff;
+   transition:0.0s;
+   height:100px;
+   height:53px;
+   padding:10px 25px;
+   }
+   .s:hover{
+   color:#fff;
+   height:53px;
+   transition:0.33s;
+   }
+   </style>
+   <br>
+   <br>
+   <br>
+   <br>
+   <center><h1>چگونه استفاده کنیم</h1>
+   <br>
+   <h2>فلا یک راه وجود دارد در گروه واتساپ عکس
+   <br>
+   بزارید
+   </h2>
+   
+   <p>
+   تا لینکی که عکس ها در ان هستند برای شما ارسال شود
+   </p>
+   <br>
+   <p>
+   در اینده سیستمی می اید تا راحت امنیت بالا تر 
+   <br>
+   داشته باشد و واتساپ در اینجا حذف خواهد شد
+   </p>
+   <br>
+   
+   <br>
+   <a class="download"style=" text-decoration: none; margin-top:13px;" href="https://wa.me/+989914723214" >
+   <samp class="s">ارسال در واتساپ</samp>
+   </a>
+   </center>
+   </body>
+   </html>
+   
+   
+  
+   
+   
+   `)
+}
+
+
